13391a1a7abf2461c66d0c9c8cd66acffaebec61
GustiGilang <gustigilang98@gmail.com>
2021-06-12 11:15:58 +0800

    Output dengan sout

---
+++ DataType.java
@@ -0,0 +1,23 @@
+public class DataType{
+ public static void main (String[]args){
+
+ String namaDepan = "Gusti";
+ String namaTengah = "Gilang";
+ String namaBelakang ="Haifa Fadillah";
+ int usia = 20;
+ int targetTahunKuliah = 4;
+ double ipk = 3.8976512;
+ char nilaiAbjad = 'A';
+ boolean tampan = true;
+
+ System.out.println ("--------------- OUTPUT ---------------");
+ System.out.println ("Nama Depan : " + namaDepan);
+ System.out.println ("Nama Tengah : " + namaTengah);
+ System.out.println ("Nama Belakang : " + namaBelakang);
+ System.out.println ("Usia : " + usia);
+ System.out.println ("Target Kuliah : " + targetTahunKuliah);
+ System.out.println ("IPK : " + ipk);
+ System.out.println ("Nilai PBO : " + nilaiAbjad);
+ System.out.println ("Tampan : " + tampan);
+ }
+}
