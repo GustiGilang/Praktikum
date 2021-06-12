a068697f733d73ff5ce43a668896c78446612c2b
GustiGilang <gustigilang98@gmail.com>
2021-06-12 11:23:21 +0800

    Output dengan MessageDialog

--- DataType.java
+++ DataType.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class DataType{
  public static void main (String[]args){
 
@@ -19,5 +21,9 @@ public class DataType{
  System.out.println ("IPK : " + ipk);
  System.out.println ("Nilai PBO : " + nilaiAbjad);
  System.out.println ("Tampan : " + tampan);
+
+ JOptionPane.showMessageDialog(null, "Hai, " + namaDepan + namaTengah + namaBelakang);
+
  }
 }
+
